d70cf6fbbedb9f5db1e6f352c692dc8ace0c3e1c
Joao Magliano <jamaglian@gmail.com>
2024-03-27 00:19:10 -0300

    - Melhoria na organiação do código

--- Atomic/tests/Feature/CompaniesTest.php
+++ Atomic/tests/Feature/CompaniesTest.php
@@ -2,10 +2,10 @@
 
 namespace Tests\Feature;
 
+use Tests\TestCase;
 use App\Models\User;
 use App\Models\Companies;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
 
 class CompaniesTest extends TestCase
 {
