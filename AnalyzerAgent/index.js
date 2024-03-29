ab3ab148cab22f816aa6a7f30fb8c77da1ac9129
Joao A <jamaglian@gmail.com>
2024-03-29 20:06:34 -0300

    - Melhorias no sistema de analise de sites -- Argumento para não ignorar css,img,js -- Salvamento de resultado em json

--- AnalyzerAgent/index.js
+++ AnalyzerAgent/index.js
@@ -1,6 +1,76 @@
+import fs from 'fs';
 import scrape from 'website-scraper'; // only as ESM, no CommonJS
 import AnalyzerAgentPlugin from './libs/AnalyzerAgentPlugin.js'; // only as ESM, no CommonJS
-const url_scrape = '';
+
+var url_scrape = '';
+var ignoreCss = true;
+var ignoreJs = true;
+var ignoreImages = true;
+const regex = /^(?:https?|ftp):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?$/;
+
+
+if(process.argv.length < 3 || !regex.test(process.argv[2])){
+  console.log("Utilize \"node index.js https://site.para.testar/ [argumentos]\"");
+  console.log("-includeAsset=css (pode utilizar 'css,js,img')")
+  process.exit(1); // Encerra o programa com código de erro
+}
+
+url_scrape = process.argv[2]
+
+if(process.argv.length > 3){
+  process.argv.forEach(element => {
+    const startIndex = element.indexOf("-includeAsset=");
+    if(startIndex !== -1){
+      const depois = element.slice(startIndex + "-includeAsset=".length);
+      if(depois.indexOf("css") !== -1){
+        ignoreCss = false;
+      }
+      if(depois.indexOf("js") !== -1){
+        ignoreJs = false;
+      }
+      if(depois.indexOf("img") !== -1){
+        ignoreImages = false;
+      }
+    }
+  });
+}
+
+var out_cli = {
+  possibleCMS: false,
+  possibleCMSType: '',
+  serverRequestTimeMap: {}
+};
+
+const jsonString = JSON.stringify(out_cli);
+const filePath = 'resultado.json';
+fs.writeFileSync(filePath, jsonString);
+
+const options = {
+  urls: [url_scrape],
+  urlFilter: (url) => url.startsWith(url_scrape), // Filter links to other websites
+  recursive: true,
+  filenameGenerator: 'bySiteStructure',
+  directory: 'teste/',
+  request: {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+    }
+  },
+  contrxt: { out_cli },
+  plugins: [ 
+    new AnalyzerAgentPlugin({
+      launchOptions: {  },
+      ignoreCss: ignoreCss,
+      ignoreJs: ignoreJs,
+      ignoreImages: ignoreImages
+    })
+  ]
+};
+
+// with async/await
+const result = await scrape(options);
+const resulta = await scrape(options);
+const resultb = await scrape(options);
 /*
 class MyPlugin {
 	apply(registerAction) {
@@ -17,24 +87,3 @@ class MyPlugin {
 	}
 }
 */
-console.log(process.argv)
-const options = {
-  urls: [url_scrape],
-  urlFilter: (url) => url.startsWith(url_scrape), // Filter links to other websites
-  recursive: true,
-  filenameGenerator: 'bySiteStructure',
-  directory: 'teste/',
-  request: {
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
-    }
-  },
-  plugins: [ 
-    new AnalyzerAgentPlugin({
-      launchOptions: {  }
-    })
-  ]
-};
-
-// with async/await
-const result = await scrape(options);
--- AnalyzerAgent/libs/AnalyzerAgentPlugin.js
+++ AnalyzerAgent/libs/AnalyzerAgentPlugin.js
@@ -1,4 +1,16 @@
+import fs from 'fs';
 import logger from './AnalyzerAgentPluginLogger.js';
+const img_extensions = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".svg",
+    ".webp",
+    // Adicione outras extensões de imagem aqui, se necessário
+];
+
 const cmsWpIdentifiersInUri = [
     'wp-content',
     'wp-includes',
@@ -7,33 +19,96 @@ const cmsWpIdentifiersInUri = [
 class AnalyzerAgentPlugin {
     
 	constructor ({
-		launchOptions = {}
+		launchOptions = {},
+        ignoreCss = true,
+        ignoreJs = true,
+        ignoreImages = true
 	} = {}) {
 		this.launchOptions = launchOptions;
+        this.ignoreCss = ignoreCss;
+        this.ignoreJs = ignoreJs;
+        this.ignoreImages = ignoreImages;
         this.possibleCMS = false;
+        this.resultadoPath = 'resultado.json';
+        this.resultadoJson = JSON.parse(fs.readFileSync(this.resultadoPath, 'utf8'));
         this.possibleCMSType = '';
+        this.possibleCMSVerison = '';
+        this.serverRequestTimeMap = this.resultadoJson.serverRequestTimeMap;
+
 		logger.info('init plugin', { launchOptions });
 	}
 
 	apply (registerAction) {
-		registerAction('beforeRequest', async ({requestOptions}) => {
-            var uri = requestOptions.headers.referer;
-            if(uri !== undefined){
-                for (const identifier in cmsWpIdentifiersInUri) {
-                    if (uri.includes(identifier)) {
-                        this.possibleCMS = true;
-                        this.possibleCMSType = 'WordPress';
+		registerAction('beforeRequest', async ({resource, requestOptions}) => {
+            var uri = resource.getUrl().split('?')[0];
+            this.identifyWp(uri);
+            if(this.ignoreCss && uri.endsWith(".css")){
+                logger.info('Ignorando css:', { uri });
+                return Promise.reject(new Error('Solicitação cancelada'));
+            }else if (this.ignoreJs && uri.endsWith(".js")){
+                logger.info('Ignorando js:', { uri });
+                return Promise.reject(new Error('Solicitação cancelada'));
+            }else if (this.ignoreImages){
+                for (const extension of img_extensions) {
+                    if (uri.endsWith(extension)){
+                        logger.info('Ignorando imagem:', { uri });
+                        return Promise.reject(new Error('Solicitação cancelada'));
                     }
                 }
             }
+            logger.info('Prosseguindo url:', { uri });
 			return {requestOptions};
 		});
         registerAction('saveResource', async ({resource}) => {return false;});
-        registerAction('afterFinish', async () => {
-            if(this.possibleCMS){
-                console.log("Possivelmente uma aplicaçao:", this.possibleCMSType)
+        registerAction('afterResponse', ({response}) => {
+            const url = response.url
+            const excludedPhases = ['dns', 'tcp', 'tls', 'request']; // Fases a serem excluídas
+            const totalExcludedTime = excludedPhases.reduce((acc, phase) => {
+                return acc + (response.timings.phases[phase] || 0); // Soma os tempos das fases excluídas
+            }, 0);
+            const serverProcessingTime = response.timings.phases.total - totalExcludedTime;
+            if(this.serverRequestTimeMap[url] !== undefined){
+                this.serverRequestTimeMap[url].push({ 
+                    serverProcessingTime: serverProcessingTime,
+                    timings: response.timings.phases
+                });
+            }else{
+                this.serverRequestTimeMap[url] = [];
+                this.serverRequestTimeMap[url].push({ 
+                    serverProcessingTime: serverProcessingTime,
+                    timings: response.timings.phases
+                });
             }
+            logger.info('Gravando tempo de resposta:', { url });
+            logger.info('O tempo de resposta do servidor foi:', { serverProcessingTime });
+            //console.log(response.timings.phases)
+            return response;
+        });
+        registerAction('afterFinish', async () => {
+            this.resultadoJson.possibleCMS = this.possibleCMS;
+            this.resultadoJson.possibleCMSType = this.possibleCMSType;
+            this.resultadoJson.serverRequestTimeMap = this.serverRequestTimeMap;
+            //console.log(this.resultadoJson);
+            const novoJsonString = JSON.stringify(this.resultadoJson, null, 2); // O terceiro argumento é para formatar a saída
+            fs.writeFileSync(this.resultadoPath, novoJsonString);
         });
 	}
+
+    identifyWp(uri){
+        if(this.possibleCMSType === ''){
+            for (const identifier of cmsWpIdentifiersInUri) {
+                if (uri.includes(identifier)) {
+                    this.possibleCMS = true;
+                    this.possibleCMSType = 'WordPress';
+                    logger.info('Possivel wordpress identificado por ' + identifier + " na url ", { uri });
+                }
+            }
+        }
+    }
+
+    async delayRequest(){
+        const time = Math.round(Math.random() * 10000);
+        await new Promise((resolve) => setTimeout(resolve, time));
+    }
 }
 export default AnalyzerAgentPlugin;
