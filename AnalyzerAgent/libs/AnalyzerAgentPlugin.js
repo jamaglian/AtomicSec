5ccb0e71666ddf556a8b602280e8086b160c9411
Joao A <jamaglian@gmail.com>
2024-04-30 19:00:02 -0300

    - Melhorias no analizador de sites

--- AnalyzerAgent/libs/AnalyzerAgentPlugin.js
+++ AnalyzerAgent/libs/AnalyzerAgentPlugin.js
@@ -64,22 +64,23 @@ class AnalyzerAgentPlugin {
         registerAction('afterResponse', ({response}) => {
             const url = response.url
             var serverProcessingTime = 0
-            if(this.serverRequestTimeMap[url] === undefined || this.resultadoJson.run > this.serverRequestTimeMap[url].length){
+            if(this.serverRequestTimeMap[url] === undefined || this.resultadoJson.run > this.serverRequestTimeMap[url].times.length){
                 if(response.timings.secureConnect !== undefined){
                     const excludeUpload = response.timings.upload - response.timings.secureConnect
                     serverProcessingTime = (response.timings.response - response.timings.secureConnect) - excludeUpload;
                 }else{
                     const excludeUpload = response.timings.upload - response.timings.connect
-                    serverProcessingTime = response.timings.response - response.timings.connect;
+                    serverProcessingTime = (response.timings.response - response.timings.connect) - excludeUpload;
                 }
                 if(this.serverRequestTimeMap[url] !== undefined){
-                    this.serverRequestTimeMap[url].push({ 
+                    this.serverRequestTimeMap[url].times.push({ 
                         serverProcessingTime: serverProcessingTime,
                         timings: this.all_times ? response.timings : null
                     });
                 }else{
-                    this.serverRequestTimeMap[url] = [];
-                    this.serverRequestTimeMap[url].push({ 
+                    this.serverRequestTimeMap[url] = {}
+                    this.serverRequestTimeMap[url].times = [];
+                    this.serverRequestTimeMap[url].times.push({ 
                         serverProcessingTime: serverProcessingTime,
                         timings: this.all_times ? response.timings : null
                     });
@@ -94,6 +95,31 @@ class AnalyzerAgentPlugin {
             this.resultadoJson.run = this.resultadoJson.run + 1;
             this.resultadoJson.possibleCMSType = this.possibleCMSType;
             this.resultadoJson.serverRequestTimeMap = this.serverRequestTimeMap;
+            var topValores = []; // array para armazenar os top 3 valores e URLs correspondentes
+
+            Object.keys(this.resultadoJson.serverRequestTimeMap).forEach(index => {
+                var media = 0;
+                this.resultadoJson.serverRequestTimeMap[index].times.forEach(unitMap => {
+                    media += unitMap.serverProcessingTime;
+                });
+                this.resultadoJson.serverRequestTimeMap[index].media = media / (this.resultadoJson.run - 1);
+
+                // Adicionar ao array dos top valores se houver menos de 3 ou se a nova média for maior que a menor dos top valores
+                if (topValores.length < 3 || this.resultadoJson.serverRequestTimeMap[index].media > topValores[2].valor) {
+                    topValores.push({ url: index, valor: this.resultadoJson.serverRequestTimeMap[index].media });
+                    // Ordenar os top valores pelo valor em ordem decrescente
+                    topValores.sort((a, b) => b.valor - a.valor);
+                    // Se a lista tiver mais de 3 valores, remover o último (menor) valor
+                    if (topValores.length > 3) {
+                        topValores.pop();
+                    }
+                }
+            });
+
+            console.log("Top 3 maiores valores:");
+            topValores.forEach(item => {
+                console.log(`URL: ${item.url}, Valor: ${item.valor}`);
+            });
             //console.log(this.resultadoJson);
             const novoJsonString = JSON.stringify(this.resultadoJson, null, 2); // O terceiro argumento é para formatar a saída
             fs.writeFileSync(this.resultadoPath, novoJsonString);
