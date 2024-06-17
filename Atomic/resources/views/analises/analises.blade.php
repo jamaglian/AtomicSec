0301b88fd988bdc0c0dbdd832de84e3c0fb4bc88
João A <jamaglian@gmail.com>
2024-06-17 10:29:21 -0300

    - Testando modal delete analise

--- Atomic/resources/views/analises/analises.blade.php
+++ Atomic/resources/views/analises/analises.blade.php
@@ -39,8 +39,8 @@
                                 @endif
                                 <td>{{ $analise->status }}</td>
                                 <td>
-                                    <a href="{{ route('analysis.analise', ['id' => $analise->id]) }}" class="btn btn-sm btn-primary">Ver</a>
-                                    <a href="#" data-toggle="modal" data-target="#exampleModal" class="btn btn-sm btn-danger">Excluir</a>
+                                    <a href="{{ route('analysis.analise', ['id' => $analise->id]) }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Ver</a>
+                                    <a href="#" data-toggle="modal" data-target="#exampleModal" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Excluir</a>
                                 </td>
                             </tr>
                             @endforeach
