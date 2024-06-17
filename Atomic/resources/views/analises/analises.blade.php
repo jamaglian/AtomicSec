c0588a8c91391c1b5eb2700b423d8059b88571ae
João A <jamaglian@gmail.com>
2024-06-17 09:22:27 -0300

    - Testando modal delete analise

--- Atomic/resources/views/analises/analises.blade.php
+++ Atomic/resources/views/analises/analises.blade.php
@@ -40,7 +40,7 @@
                                 <td>{{ $analise->status }}</td>
                                 <td>
                                     <a href="{{ route('analysis.analise', ['id' => $analise->id]) }}" class="btn btn-sm btn-primary">Ver</a>
-                                    <a href="" class="btn btn-sm btn-danger">Excluir</a>
+                                    <a href="#" data-toggle="modal" data-target="#exampleModal" class="btn btn-sm btn-danger">Excluir</a>
                                 </td>
                             </tr>
                             @endforeach
@@ -50,4 +50,23 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
 </x-app-layout>
